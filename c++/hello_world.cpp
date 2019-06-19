4bc0cf76f3c91424272ca56ebc824a00bec9161a
snilloO <vega0.0@outlook.com>
2019-06-19 17:10:39 +0800

    update notes

---
+++ c++/hello_world.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <limits>
+using namespace std;
+int main(){
+    cout<< "Hello Word!" << endl;
+    int a(4.5);
+    cout<< a <<endl;
+    /*cin.clear(); // reset any error flags
+    cin.ignore(numeric_limits<streamsize>::max(), '\n'); // ignore any characters in the input buffer until we find an enter character
+    cin.get(); // get one more char from the user */
+    return 0;
+}
